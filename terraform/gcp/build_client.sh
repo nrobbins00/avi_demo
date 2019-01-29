2a4fe38e212da84406b0d2efef1b891e559bf73e
Nick Robbins <17030717+nrobbins00@users.noreply.github.com>
2019-01-29 14:43:01 -0600

    Fix errors with missing floating ip

--- terraform/gcp/build_client.sh
+++ terraform/gcp/build_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 sudo yum install -y epel
 sudo yum groupinstall -y 'Development Tools'
 sudo yum install -y perl-JSON-PP
@@ -13,7 +13,14 @@ until `curl -s -k -u '${avi_username}:${avi_password}' https://${avi_ip}/api/vir
     echo "sleeping until VS ready"
     sleep 15s
     done     
-echo "VS ready, proceed"    
+echo "VS ready, proceed"
+echo "checking existence of floating on VIP"
+VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://10.155.0.9/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
+while [ -z "$$VIP_IP" ]; do
+	sleep 15s
+	VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://10.155.0.9/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
+	done
+echo "testing against $${VIP_IP}"`
 VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://${avi_ip}/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
 echo "testing against $${VIP_IP}"
 nohup /usr/bin/locust -f /tmp/locustfile.py --no-web -c 100 -r 25 -t 30m -H http://$${VIP_IP} --logfile=/tmp/locustlogs.txt &
