de5dd8935c07b003eff59528555dcfd23d639f44
Nick Robbins <17030717+nrobbins00@users.noreply.github.com>
2019-01-29 14:50:23 -0600

    Typo

--- terraform/gcp/build_client.sh
+++ terraform/gcp/build_client.sh
@@ -15,12 +15,12 @@ until `curl -s -k -u '${avi_username}:${avi_password}' https://${avi_ip}/api/vir
     done     
 echo "VS ready, proceed"
 echo "checking existence of floating on VIP"
-VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://10.155.0.9/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
+VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://${avi_ip}/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
 while [ -z "$$VIP_IP" ]; do
 	sleep 15s
-	VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://10.155.0.9/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
+	VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://${avi_ip}/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
 	done
-echo "testing against $${VIP_IP}"`
+echo "testing against $${VIP_IP}"
 VIP_IP=`curl -s -k -u 'admin:C0mplexP@ssw0rd         # replace with 'examplePass' instead' https://${avi_ip}/api/virtualservice/?name=Web-FrontEnd | json_pp | grep -A3 floating | grep addr | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | awk '{$1=$1};1'`
 echo "testing against $${VIP_IP}"
 nohup /usr/bin/locust -f /tmp/locustfile.py --no-web -c 100 -r 25 -t 30m -H http://$${VIP_IP} --logfile=/tmp/locustlogs.txt &
